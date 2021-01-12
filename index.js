c3261a5151f1dd5d4bc9d89cac0a55aa7e3b730a
Omar Al Hennawi <omar504969@gmail.com>
2021-01-12 18:48:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function logWhisper(string){
   console.log(string .toLowerCase())
 }
 function sayHiToGrandma(string){
-  if(string === .toLowerCase){
+  if(string === lowcase){
     return "I can't hear you!"
   }
-  else if(string === .toUpperCase){
+  else if(string === uppercase){
     return "YES INDEED!"
   }
   else if(srting === "I love you, Grandma"){
