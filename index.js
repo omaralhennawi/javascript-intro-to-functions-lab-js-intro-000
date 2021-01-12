a1354b944764c8be1ad609323ec1d3b02d9e97ee
Omar Al Hennawi <omar504969@gmail.com>
2021-01-12 19:16:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,13 +11,17 @@ function logWhisper(string){
   console.log(string .toLowerCase())
 }
 function sayHiToGrandma(string){
-  if(string.toLowerCase() ){
-    return "I can't hear you!"
-  }
-  else if (string.toUpperCase() ){
-    return "YES INDEED!"
-  }
-  else if (string === 'I love you, Grandma.'){
-    return "I love you, too."
+  switch(string){
+    case string.toLowerCase:
+      return "I can't hear you!";
+      break;
+    case string.toUpperCase :
+      return "YES INDEED!";
+      break;
+    case string === "I love you, Grandma."
+      return "I love you, too.";
+      break;
   }
 }
+
+  
